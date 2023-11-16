4bc4c2608ffe0b9c880264e3f31e9786cf136c2e
dannyfrelink <danny-frelink@hotmail.com>
2023-11-16 16:43:49 +0100

    update: add styling to plane images to make them all the same size

--- client/src/components/PlaneImage.js
+++ client/src/components/PlaneImage.js
@@ -3,21 +3,69 @@ const PlaneImage = ({ model }) => {
     try {
       switch (model) {
         case "A319":
-          return <img src="planes/A319.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/A319.jpeg"
+              alt={model}
+            />
+          );
         case "A320":
-          return <img src="planes/A320.avif" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/A320.avif"
+              alt={model}
+            />
+          );
         case "A321":
-          return <img src="planes/A321.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/A321.jpeg"
+              alt={model}
+            />
+          );
         case "A332":
-          return <img src="planes/A332.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/A332.jpeg"
+              alt={model}
+            />
+          );
         case "A333":
-          return <img src="planes/A333.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/A333.jpeg"
+              alt={model}
+            />
+          );
         case "B77W":
-          return <img src="planes/B77W.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/B77W.jpeg"
+              alt={model}
+            />
+          );
         case "B737":
-          return <img src="planes/B737.webp" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/B737.webp"
+              alt={model}
+            />
+          );
         case "B738":
-          return <img src="planes/B738.jpeg" alt={model} />;
+          return (
+            <img
+              className="h-56 w-full object-cover object-center"
+              src="planes/B738.jpeg"
+              alt={model}
+            />
+          );
         default:
           return <p>No image available</p>;
       }
